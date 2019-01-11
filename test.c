061ba3983c6cbcff55a14fb81ef0a5232845c3ce
Example <example@mail.com>
2019-01-10 22:12:56 -0800

    "Add a new line for testing"

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+<<< Add a new line >>>
 <<< Use Configuration Wizard in Context Menu >>>
 The following optional text marks the end of the Configuration Wizard section.
 <<< end of configuration section >>>
